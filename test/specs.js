372ce2ca5982c9fec1084772c50cfb3fc67a0aae
Thomas Hallock <thomas@1stdibs.com>
2016-01-06 18:35:03 -0500

    got spec working

--- test/specs.js
+++ test/specs.js
@@ -1,16 +1,20 @@
 "use strict";
 var ValidModel = require('../index');
+var sinon = require('sinon');
+var assert = require('assert');
 describe("valid-model", function () {
     var BuggedValidModel;
     var validModel;
+    var validate;
     beforeEach(function () {
+        validate = sinon.spy();
         BuggedValidModel = ValidModel.extend({
-            validate: jasmine.createSpy("validation")
+            validate: validate
         });
         validModel = new BuggedValidModel();
     })
     it("should default to validate:true for the options passed to Model.prototype.set", function () {
         validModel.set('name', 'thomas');
-        expect(validModel.validate).toHaveBeenCalled();
+        assert(validate.called);
     });
 });
